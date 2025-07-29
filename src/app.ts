46ac090a65f48d3a658959bc00649a312cabcb77
Graygorius Arif <rayarif09@gmail.com>
2025-07-29 15:23:53 +0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,44 @@
+import express, { Express } from "express";
+import cors from "cors";
+import "reflect-metadata";
+import { PORT } from "./config/env";
+import { SampleRouter } from "./modules/sample/sample.router";
+import { errorMiddleware } from "./middlewares/error.middleware";
+import { AuthRouter } from "./modules/auth/auth.router";
+import { BlogRouter } from "./modules/blog/blog.router";
+
+export class App {
+  app: Express;
+  
+  constructor() {
+    this.app = express();
+    this.configure();
+    this.routes();
+    this.handleError();
+  }
+
+  private configure() {
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  private routes() {
+    const sampleRouter = new SampleRouter();
+    const authRouter = new AuthRouter();
+    const blogRouter = new BlogRouter();
+
+    this.app.use("/samples", sampleRouter.getRouter());
+    this.app.use("/auth", authRouter.getRouter());
+    this.app.use("/blogs", blogRouter.getRouter());
+  }
+
+  private handleError() {
+    this.app.use(errorMiddleware);
+  }
+
+  public start() {
+    this.app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  }
+}
---
+++ src/modules/auth/auth.router.ts
@@ -0,0 +1,33 @@
+import { Router } from "express";
+import { AuthController } from "./auth.controller";
+import { validateBody } from "../../middlewares/validate.middleware";
+import { RegisterDTO } from "./dto/register.dto";
+import { LoginDTO } from "./dto/login.dto";
+
+export class AuthRouter {
+  private authController: AuthController;
+  private router: Router;
+  
+  constructor() {
+    this.router = Router();
+    this.authController = new AuthController();
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes = () => {
+    this.router.post(
+      "/register",
+      validateBody(RegisterDTO),
+      this.authController.register
+    );
+    this.router.post(
+      "/login",
+      validateBody(LoginDTO),
+      this.authController.login
+    );
+  };
+
+  getRouter = () => {
+    return this.router;
+  };
+}
